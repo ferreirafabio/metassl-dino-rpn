dafd7b272c1c22a48c8fef512173ef8c7d3d387c
Diane Wagner <wagnerd@informatik.uni-freiburg.de>
2022-04-29 11:26:05 +0200

    Update submission script

--- cluster/submit_dino_neps.sh
+++ cluster/submit_dino_neps.sh
@@ -3,11 +3,11 @@
 #SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:8
 #SBATCH -J dinoneps
-#SBATCH -t 00:15:00
+#SBATCH -t 23:59:59
 #SBATCH --array 0-99%2
 
 pip list
 
 source activate dino
 
-python -m main_dino --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40 --is_neps_run --epochs 50
+python -m main_dino --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40 --is_neps_run
