6344e7763db8ab1f8df8bc972dbc5d64d45566f3
fabioferreira <fabiof@cs.stanford.edu>
2022-05-10 12:49:54 +0200

    prepared cluster script with Sam for distributed NEPS

--- cluster/submit_dino_neps_fabio_sam.sh
+++ cluster/submit_dino_neps_fabio_sam.sh
@@ -4,9 +4,16 @@
 #SBATCH -J dino_neps_hpo
 #SBATCH -t 5-23:59:59  # 23:59:59
 #SBATCH --array 0-9999%10
+echo "HI"
 
 source /home/ferreira/.profile
 source activate dino
 
-python -m torch.distributed.launch --use_env --nproc_per_node=8 --nnodes=1 main_dino.py --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/ferreira-dino/metassl-dino/experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40 --is_neps_run --epochs 100 --world_size 8 --gpu 8
+mkdir -p /tmp/dino_communication
+
+filename=/tmp/dino_communication/$(openssl rand -hex 12)
+
+python -m torch.distributed.launch --use_env --nproc_per_node=8 --nnodes=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/ferreira-dino/metassl-dino/experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40 --is_neps_run --epochs 100 --world_size 8 --gpu 8
+
+rm filename
 
