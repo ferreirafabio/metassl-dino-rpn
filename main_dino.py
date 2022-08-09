de17f09e0c923ed200cfc587067332890e6e92ae
fabioferreira <fabiof@cs.stanford.edu>
2022-08-09 14:42:51 -0500

    debugging

--- main_dino.py
+++ main_dino.py
@@ -390,7 +390,7 @@ def train_dino(rank, working_directory, previous_working_directory, args, hyperp
     print("Starting DINO training !")
 
     with torch.cuda.amp.autocast(fp16_scaler is not None):
-        rpn = RPN(ResNetRPN('resnet18').cuda()).cuda()
+        rpn = RPN(backbone=ResNetRPN('resnet18').cuda()).cuda()
 
     if args.is_neps_run:
         end_epoch = hyperparameters["epoch_fidelity"]
@@ -505,7 +505,8 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
         
         # teacher and student forward passes + compute dino loss
         with torch.cuda.amp.autocast(fp16_scaler is not None):
-            images = rpn(images, fp16_scaler is not None)
+            print(images.is_cuda)
+            images = rpn(images)
             teacher_output = teacher(images[:2])  # only the 2 global views pass through the teacher
             student_output = student(images)
             loss = dino_loss(student_output, teacher_output, epoch)
