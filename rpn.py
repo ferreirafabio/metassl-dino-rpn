95dcd9c2c40769099d0133e30118aa9d362970f2
fabioferreira <fabiof@cs.stanford.edu>
2022-08-10 14:04:51 -0500

    improving speed

--- rpn.py
+++ rpn.py
@@ -61,11 +61,11 @@ class RPN(nn.Module):
     
         self.modules_g1 = transforms.Compose(
             [
-                # transforms.ToPILImage(),
+                transforms.ToPILImage(),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8),
                 transforms.RandomGrayscale(p=0.2),
-                # utils.GaussianBlur(1.0),
+                utils.GaussianBlur(1.0),
                 # kornia.augmentation.RandomGaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0), p=1.0),
                 self.normalize,
                 ]
@@ -73,13 +73,13 @@ class RPN(nn.Module):
         
         self.modules_g2 = transforms.Compose(
             [
-                # transforms.ToPILImage(),
+                transforms.ToPILImage(),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8),
                 transforms.RandomGrayscale(p=0.2),
-                # utils.GaussianBlur(1.0),
+                utils.GaussianBlur(1.0),
                 # kornia.augmentation.RandomGaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0), p=1.0),
-                # utils.Solarization(0.2),
+                utils.Solarization(0.2),
                 # transforms.RandomSolarize(threshold=128, p=0.2),
                 # kornia.augmentation.RandomSolarize(p=0.2),
                 self.normalize,
@@ -88,11 +88,11 @@ class RPN(nn.Module):
         
         self.modules_l = transforms.Compose(
             [
-                # transforms.ToPILImage(),
+                transforms.ToPILImage(),
                 transforms.RandomHorizontalFlip(p=0.5),
                 transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8),
                 transforms.RandomGrayscale(p=0.2),
-                # utils.GaussianBlur(0.5),
+                utils.GaussianBlur(0.5),
                 # transforms.RandomApply(transforms.GaussianBlur(kernel_size=5), p=0.5),
                 # kornia.augmentation.RandomGaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0), p=0.5),
                 self.normalize,
