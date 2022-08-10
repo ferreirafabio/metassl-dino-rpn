f1548eb5985626d8e196e82b9ad344b7cd1c37d9
fabioferreira <fabiof@cs.stanford.edu>
2022-08-10 12:27:04 -0500

    improving speed

--- rpn.py
+++ rpn.py
@@ -98,6 +98,7 @@ class RPN(nn.Module):
         # g_view1_tensors, g_view2_tensors, l_view1_tensors, l_view2_tensors = [], [], [], []
         views = []
         g_views1_cropped_batch, g_views2_cropped_batch, l_views1_cropped_batch, l_views2_croped_batch = [], [], [], []
+        print(imgs)
         imgs = torch.Tensor(imgs)
         print(imgs.size())
         embs = self.backbone(imgs)
