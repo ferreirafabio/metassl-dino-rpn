a2e8452e493d1b63c3a3a9764be944c2a076e645
fabioferreira <fabiof@cs.stanford.edu>
2022-08-10 16:35:06 -0500

    improving speed

--- main_dino.py
+++ main_dino.py
@@ -543,6 +543,8 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
                 param_norms = utils.clip_gradients(student, args.clip_grad)
             utils.cancel_gradients_last_layer(epoch, student,
                                               args.freeze_last_layer)
+            
+            print(rpn.module.backbone.backbone.fc.weight)
             optimizer.step()
             
             # RPN
