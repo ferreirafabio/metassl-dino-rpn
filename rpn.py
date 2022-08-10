19d0fb0476dd2355123f5554dcce7bbe7cb54b4f
fabioferreira <fabiof@cs.stanford.edu>
2022-08-10 14:06:44 -0500

    improving speed

--- rpn.py
+++ rpn.py
@@ -56,6 +56,7 @@ class RPN(nn.Module):
         self.local2_fc = nn.Linear(256, 2)
         
         self.normalize = transforms.Compose([
+            transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ])
     
