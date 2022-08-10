07869b3ef33168bf5e4f1734a5bf81c0fd2dbf41
fabioferreira <fabiof@cs.stanford.edu>
2022-08-10 15:08:39 -0500

    improving speed

--- main_dino.py
+++ main_dino.py
@@ -342,7 +342,8 @@ def train_dino(rank, working_directory, previous_working_directory, args, hyperp
     params_groups = utils.get_params_groups(student)
     if args.optimizer == "adamw":
         # optimizer = torch.optim.AdamW(params_groups)  # to use with ViTs
-        optimizer = torch.optim.AdamW(params_groups + rpn.parameters())  # to use with ViTs
+        print(params_groups)
+        optimizer = torch.optim.AdamW(params_groups + list(rpn.parameters()))  # to use with ViTs
     elif args.optimizer == "sgd":
         optimizer = torch.optim.SGD(params_groups, lr=0, momentum=0.9)  # lr is set by scheduler
     elif args.optimizer == "lars":
