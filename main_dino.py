46076af5e72f80b34be1bd266efca899f525ef30
fabioferreira <fabiof@cs.stanford.edu>
2022-08-12 16:25:15 -0500

    spatial transformer network

--- main_dino.py
+++ main_dino.py
@@ -346,6 +346,9 @@ def train_dino(rank, working_directory, previous_working_directory, args, hyperp
     all_params = student_params + list(rpn.parameters())
     params_groups[0]['params'] = all_params
 
+    for name, param in rpn.named_parameters:
+        print(name, param)
+    # print(list(rpn.parameters()))
     # for name, param in rpn.named_parameters():
     #     if param.requires_grad:
     #         print(name)
