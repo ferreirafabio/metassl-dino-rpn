47a989f111a77e3ae36febc9cd3490234b5cb5ae
fabioferreira <fabiof@cs.stanford.edu>
2022-08-31 11:35:58 +0200

    profiling due to cuda memory leak

--- main_dino.py
+++ main_dino.py
@@ -566,9 +566,7 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
         # teacher and student forward passes + compute dino loss
         with torch.cuda.amp.autocast(fp16_scaler is not None):
             images = rpn(images)
-
-            continue
-
+            # continue
             teacher_output = teacher(images[:2])  # only the 2 global views pass through the teacher
             student_output = student(images)
             loss = dino_loss(student_output, teacher_output, epoch)
