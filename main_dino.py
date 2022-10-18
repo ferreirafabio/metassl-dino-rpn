1441f124a810c10e0dc2102a678a535b03927195
fabioferreira <fabiof@cs.stanford.edu>
2022-10-18 14:18:01 +0200

    testing inversion of gradients

--- main_dino.py
+++ main_dino.py
@@ -524,11 +524,9 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
                 if args.test_mode:
                     inverted_grads_l1 = rpn.module.transform_net.fc_localization_local1.linear2.weight.grad.cpu().data.numpy()
 
-                    images_cpu = [img.cpu().data.numpy() for img in images]
-                    images_test_mode_cpu = [img.cpu().data.numpy() for img in images_test_mode]
-                    
-                    print(f"inputs are equal: {np.isclose(images_cpu, images_test_mode_cpu)}")
-                    
+                    for img, img_test in zip(images, images_test_mode):
+                        print(f"inputs are equal: {np.isclose(img.cpu().data.numpy(), img_test.cpu().data.numpy())}")
+                
                     images = rpn(images_test_mode, invert_rpn_gradients=False)
                     teacher_output = teacher(images[:2])  # only the 2 global views pass through the teacher
                     student_output = student(images)
