bug fix: gradient was inverted in the localization net AND in the head. Now only in the head

main_dino.py
@@ -20,10 +20,8 @@ import math
 import json
 import logging
 from pathlib import Path
-import pickle
 import copy
 import numpy as np
-from PIL import Image
 import torch
 import torch.nn as nn
 import torch.distributed as dist
@@ -38,9 +36,8 @@ import utils
 from utils import custom_collate, SummaryWriterCustom
 import vision_transformer as vits
 from vision_transformer import DINOHead
-from functools import partial
-from rpn import AugmentationNetwork
-from rpn import STN
+from stn import AugmentationNetwork
+from stn import STN
 
 torchvision_archs = sorted(name for name in torchvision_models.__dict__
     if name.islower() and not name.startswith("__")

stn.py
@@ -90,10 +90,7 @@ class LocalizationNet(nn.Module):
             xs = self.maxpool2d(F.leaky_relu(self.conv2d_deep_bn1(self.conv2d_deep1(xs))))
             xs = self.maxpool2d(F.leaky_relu(self.conv2d_deep_bn2(self.conv2d_deep2(xs))))
 
-        if self.invert_rpn_gradients:
-            xs = grad_reverse(self.avgpool(F.leaky_relu(self.conv2d_bn2(self.conv2d_2(xs)))))
-        else:
-            xs = self.avgpool(F.leaky_relu(self.conv2d_bn2(self.conv2d_2(xs))))
+        xs = self.avgpool(F.leaky_relu(self.conv2d_bn2(self.conv2d_2(xs))))
             
         return xs
 